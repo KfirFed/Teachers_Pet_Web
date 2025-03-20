ffe216d323a43fe87521d4a35a57d70e69e62262
KfirFed <xd.KFederovsky@idf.il>
2025-03-20 14:43:57 +0200

    feat/landing-page

--- teachers-pet-client/src/Router.tsx
+++ teachers-pet-client/src/Router.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
 
-// Main App Component
 const Router: React.FC = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/landing" />} />
+      <Route path="/landing" element={<LandingPage/>} />
+      <Route path="*" element={<Navigate to="/landing" />} />
     </Routes>
   );
 };
