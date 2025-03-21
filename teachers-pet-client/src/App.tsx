a417a85da632330159fe43545eea24071877687a
ofri yaakov <ofriyaakov@gmail.com>
2025-03-21 17:11:29 +0200

    add user context

--- teachers-pet-client/src/App.tsx
+++ teachers-pet-client/src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Router from './Router';
+import { UserProvider } from './context';
 
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <UserProvider>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </UserProvider>
     </div>
   );
 }
---
+++ teachers-pet-client/src/context/UserContext.tsx
@@ -0,0 +1,66 @@
+import React, { createContext, useState, useCallback } from "react";
+
+export interface ConnectedUser {
+    id: string;
+    name: string;
+    email: string;
+    profileImage: string;
+    accessToken: string;
+    refreshToken: string;
+}
+
+interface UserContextType {
+    connectedUser: ConnectedUser | null;
+    updateConnectedUser: (user: ConnectedUser) => void;
+    resetConnectedUser: () => void;
+    isAuthenticated: boolean;
+}
+
+const userContextValue: UserContextType = {
+    connectedUser: null,
+    updateConnectedUser: () => { },
+    resetConnectedUser: () => { },
+    isAuthenticated: false,
+};
+export const UserContext = createContext<UserContextType>(userContextValue);
+
+interface UserProviderProps {
+    children: React.ReactNode;
+}
+
+export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+    const [connectedUser, setConnectedUser] = useState<ConnectedUser | null>(
+        () => {
+            const storedUser = localStorage.getItem("user");
+            if (storedUser) {
+                localStorage.setItem("accessToken", JSON.parse(storedUser).accessToken);
+                localStorage.setItem("refreshToken", JSON.parse(storedUser).refreshToken);
+                return JSON.parse(storedUser);
+            }
+            return null;
+        }
+    );
+
+    const updateConnectedUser = useCallback((user: ConnectedUser) => {
+        setConnectedUser(user);
+        localStorage.setItem("accessToken", user.accessToken);
+        localStorage.setItem("refreshToken", user.refreshToken);
+        localStorage.setItem("user", JSON.stringify(user));
+    }, []);
+
+    const resetConnectedUser = useCallback(() => {
+        setConnectedUser(null);
+        localStorage.removeItem("user");
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+    }, []);
+
+    const userProviderValue = {
+        connectedUser,
+        updateConnectedUser,
+        resetConnectedUser,
+        isAuthenticated: !!connectedUser,
+    };
+
+    return <UserContext.Provider value={userProviderValue}>{children}</UserContext.Provider>;
+};
