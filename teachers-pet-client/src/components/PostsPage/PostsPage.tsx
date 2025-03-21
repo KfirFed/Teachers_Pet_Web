25fed82b66d311799a26c321a644ea3c73bd1f1f
ofri yaakov <ofriyaakov@gmail.com>
2025-03-21 22:08:32 +0200

    filter to posts page

--- teachers-pet-client/src/components/PostsPage/PostsPage.tsx
+++ teachers-pet-client/src/components/PostsPage/PostsPage.tsx
@@ -1,17 +1,28 @@
 import { PostsList } from "../Posts/PostsList";
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import { IconButton } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Post } from "../../types/Post";
 
 export const PostsPage: React.FC = () => {
+    const [isFiltered, setIsFiltered] = useState<boolean>(false);
+    const [postsData, setPostsData] = useState<Post[]>([]);
+
+    // todo: add axios
+
+    useEffect(() => {
+        //todo: add posts data
+        setPostsData([])
+    }, [isFiltered]);
 
     return (
         <div>
             <IconButton
-                aria-label='show comments'
-                onClick={() => { }}>
+                aria-label='only my posts'
+                onClick={() => setIsFiltered(!isFiltered)}>
                 <FilterAltIcon />
             </IconButton>
-            <PostsList postsData={[]} />
+            <PostsList postsData={postsData} />
         </div>
     );
 };
