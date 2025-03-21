7604b24f13dc5e109a94b90798c34b1e034dfc03
ofri yaakov <ofriyaakov@gmail.com>
2025-03-21 17:41:43 +0200

    add login requset

---
+++ teachers-pet-client/src/axios/auth.ts
@@ -0,0 +1,17 @@
+import axiosConnection from "./axiosConnection";
+import { ConnectedUser } from "../context/UserContext";
+
+interface loginInfo {
+    email: string;
+    password: string;
+}
+
+const AUTH_ROUTE = "/auth";
+
+export const login = async (loginInfo: loginInfo): Promise<ConnectedUser> => {
+    try {
+        return (await axiosConnection.post(`${AUTH_ROUTE}/login`, loginInfo)).data;
+    } catch (error: any) {
+        throw new Error(error.response?.data?.message || "Login failed");
+    }
+};
