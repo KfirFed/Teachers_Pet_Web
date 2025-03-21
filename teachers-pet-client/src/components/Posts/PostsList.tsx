3bef6cc66f5b11f6bc94a458f403fc1928070c36
ofri yaakov <ofriyaakov@gmail.com>
2025-03-21 21:54:31 +0200

    posts list component

---
+++ teachers-pet-client/src/components/Posts/PostsList.tsx
@@ -0,0 +1,28 @@
+import { Post } from "./../../types/Post";
+import { SinglePost } from "./SinglePost";
+
+export interface PostsList {
+    postsData: Post[];
+    direction?: "column" | "row";
+}
+
+export const PostsList: React.FC<PostsList> = ({
+    postsData,
+    direction = "column",
+}) => {
+    return (
+        <div
+            style={{
+                display: "grid",
+                gridTemplateColumns: direction === "row" ? "repeat(3, 1fr)" : "1fr",
+                gap: "16px",
+            }}>
+            {postsData.map((post: Post) => (
+                <SinglePost
+                    key={post?.id}
+                    post={post}
+                />
+            ))}
+        </div>
+    );
+};
