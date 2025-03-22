a36effe8db2ef9253459f08257bebcc5b2935281
ofri yaakov <ofriyaakov@gmail.com>
2025-03-22 03:18:49 +0200

    add google provider

--- teachers-pet-client/src/App.tsx
+++ teachers-pet-client/src/App.tsx
@@ -2,15 +2,21 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Router from './Router';
 import { UserProvider } from './context';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
   return (
     <div className="app">
-      <UserProvider>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
-      </UserProvider>
+      <GoogleOAuthProvider
+        clientId={
+          "278201745490-b042re8r90p48mapteqvchtv0da2tsf9.apps.googleusercontent.com"
+        }>
+        <UserProvider>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </UserProvider>
+      </GoogleOAuthProvider>
     </div>
   );
 }
