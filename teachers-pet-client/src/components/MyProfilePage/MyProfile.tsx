d27f27dd2b317de5c0c778b9f579ac977c4dad98
KfirFed <xd.KFederovsky@idf.il>
2025-03-22 03:02:08 +0200

    fix

--- teachers-pet-client/src/components/MyProfilePage/MyProfile.tsx
+++ teachers-pet-client/src/components/MyProfilePage/MyProfile.tsx
@@ -1,22 +1,69 @@
-import { Alert, Box, Button, Card, CardContent, TextField, Typography } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  styled,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useContext, useState } from "react";
 import { askAI } from "../../axios/AI";
 import styles from "./MyProfile.module.css";
 import { UserContext } from "../../context/UserContext";
+import { axiosCreateImage } from "../../axios/Images";
 
 export const MyProfile: React.FC = () => {
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
   const { connectedUser } = useContext(UserContext);
   const [username, setUsername] = useState(connectedUser?.username || "");
-  const [profileImage, setProfileImage] = useState(connectedUser?.profileImage || "");
+  const [profileImage, setProfileImage] = useState(
+    connectedUser?.profileImage || ""
+  );
   const [loading, setLoading] = useState(false);
+  const [imageFile, setImageFile] = useState<File>();
+  const [imageUrl, setImageUrl] = useState<string>(connectedUser?.profileImage || "");
+  const [image, setImage] = useState<string>(connectedUser?.profileImage || "");
 
-  console.log('connectedUser', connectedUser)
-  console.log(connectedUser?.profileImage);
+  console.log("check", imageUrl);
 
+  const ImageView = styled(Box)(({ theme }) => ({
+    width: "100%",
+    height: 200,
+    backgroundColor: theme.palette.grey[200],
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+  }));
 
-  // setUsername(connectedUser?.name || "");
-  // setProfileImage(connectedUser?.profileImage || "");
+  const onImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const newURL = URL.createObjectURL(e.target.files[0]);
+      setImage(newURL);
+      setImageFile(e.target.files[0]);
+    }
+  };
+
+  const uploadImage = async (file: File) => {
+    const ImageData = new FormData();
+    if (file) {
+      ImageData.append("file", file);
+      try {
+        const response = await axiosCreateImage(ImageData);
+        setImageUrl((response.data.url as string) || "");
+        console.log(setImageUrl);
+        return imageUrl;
+      } catch (err) {
+        console.log(err);
+        return;
+      }
+    }
+    return imageUrl;
+  };
 
   return (
     <Card
@@ -56,17 +103,26 @@ export const MyProfile: React.FC = () => {
               helperText={errors.username}
               // className={styles.input}
             />
-            <TextField
-              fullWidth
-              margin="normal"
-              type="file"
-              value={profileImage}
-              onChange={(e) => setProfileImage(e.target.value)}
-              required
-              error={!!errors.profileImage}
-              helperText={errors.profileImage}
-              // className={styles.input}
-            />
+            <label
+              htmlFor="photo-upload"
+              style={{
+                cursor: "pointer",
+              }}
+            >
+              <input
+                id="photo-upload"
+                type="file"
+                accept="image/*"
+                onChange={onImageUpload}
+                style={{ display: "none" }}
+              />
+              {imageUrl ? (
+                <CardMedia component="img" height="194" image={imageUrl} />
+              ) : (
+                <ImageView />
+              )}
+            </label>
+
             {errors.submit && (
               <Alert severity="error" sx={{ mt: 2 }}>
                 {errors.submit}
