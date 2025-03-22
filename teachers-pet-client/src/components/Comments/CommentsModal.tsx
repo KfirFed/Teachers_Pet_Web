d91784ca1d514316557d2f5ae491afa27c49fd86
ofri yaakov <ofriyaakov@gmail.com>
2025-03-22 16:05:27 +0200

    add comments modal

---
+++ teachers-pet-client/src/components/Comments/CommentsModal.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import {
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    TextField,
+    Box,
+    Button,
+} from "@mui/material";
+
+interface CommentsModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    postId: string
+}
+
+const CommentsDialog: React.FC<CommentsModalProps> = ({
+    isOpen,
+    onClose,
+}) => {
+    const [newComment, setNewComment] = useState<string>("");
+
+    const onSave = async (e: React.FormEvent) => { };
+
+    return (
+        <Dialog open={isOpen} onClose={onClose} maxWidth='sm' fullWidth>
+            <DialogTitle sx={{ m: 0, p: 2 }}>
+                Comments
+                <Button
+                    onClick={onClose}
+                    sx={{
+                        position: "absolute",
+                        right: 8,
+                        top: 8,
+                        color: (theme) => theme.palette.grey[500],
+                    }}>
+                    Close
+                </Button>
+            </DialogTitle>
+
+            <DialogContent>
+                <Box sx={{ mb: 2, maxHeight: "60vh", overflowY: "auto" }}>
+                    {/* all comments here */}
+                </Box>
+                <Box
+                    component='form'
+                    onSubmit={onSave}
+                    sx={{
+                        display: "flex",
+                        gap: 1,
+                        position: "sticky",
+                        bottom: 0,
+                        bgcolor: "background.paper",
+                        pt: 2,
+                        flexDirection: "row-reverse",
+                    }}>
+                    <TextField
+                        value={newComment}
+                        onChange={(e) => setNewComment(e.target.value)}
+                        placeholder='הוספת תגובה'
+                        variant='outlined'
+                        size='small'
+                        fullWidth
+                        autoFocus
+                    />
+                    <Button
+                        type='submit'
+                        variant='contained'
+                        disabled={!newComment.trim()}>
+                        publish
+                    </Button>
+                </Box>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+export default CommentsDialog;
