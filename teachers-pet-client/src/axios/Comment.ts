9c3160369846f19fd62e41a42fa687ee4a90d53b
ofri yaakov <ofriyaakov@gmail.com>
2025-03-22 16:49:08 +0200

    add create comment to axios

--- teachers-pet-client/src/axios/Comment.ts
+++ teachers-pet-client/src/axios/Comment.ts
@@ -1,5 +1,5 @@
 import axiosConnection from "./AxiosConnection";
-import { Comment } from "../types/Comment";
+import { Comment, CreateComment } from "../types/Comment";
 
 const COMMENT_ROUTE = "/comments";
 
@@ -8,5 +8,13 @@ const axiosGetAllCommentsByPostId = async (postId: string): Promise<Comment[]> =
         return (await axiosConnection.get(`${COMMENT_ROUTE}/post/:${postId}`)).data;
     } catch (error: any) {
         throw new Error(error.response?.data?.message || "Can't get comments");
+    }
+};
+
+const axiosCreatePost = async (comment: CreateComment, accessToken: string): Promise<Response> => {
+    try {
+        return (await axiosConnection.post(`${COMMENT_ROUTE}/`, comment, { headers: { authorization: `Bearer ${accessToken}` } }))
+    } catch (error: any) {
+        throw new Error(error.response?.data?.message || "Can't create comment");
     }
 };
--- teachers-pet-client/src/types/Comment.ts
+++ teachers-pet-client/src/types/Comment.ts
@@ -1,6 +1,14 @@
-export interface Comment {
+interface Comment {
     _id: string;
     postId: string;
     content: string;
     ownerId: string
 }
+
+interface CreateComment {
+    postId: string;
+    content: string;
+    ownerId: string
+}
+
+export type { Comment, CreateComment }
