8c0563736ac03676d0313bcff8dcf38d2b1109f2
KfirFed <xd.KFederovsky@idf.il>
2025-03-22 16:27:55 +0200

    fix/login-after

--- teachers-pet-client/src/components/Register/Register.tsx
+++ teachers-pet-client/src/components/Register/Register.tsx
@@ -6,7 +6,7 @@ import styles from "./Register.module.css";
 import { useContext } from "react";
 import { UserContext } from "../../context/UserContext";
 import { ConnectedUser } from "../../types/User";
-import { register } from "../../axios/Auth";
+import { login, register } from "../../axios/Auth";
 import { axiosCreateImage } from "../../axios/Images";
 
 export const Register: React.FC = () => {
@@ -20,12 +20,13 @@ export const Register: React.FC = () => {
     profileImage: string
   ) => {
     try {
-      const connectedUser: ConnectedUser = await register(
+       await register(
         email,
         username,
         password,
         profileImage
       );
+      const connectedUser: ConnectedUser = await login(email, password);
       if (!!connectedUser) {
         updateConnectedUser(connectedUser);
         navigate("/posts");
